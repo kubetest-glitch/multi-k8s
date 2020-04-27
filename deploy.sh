fe6e0268879b992c0e0b59e4bd0b355b8ca23f96
David Zadrazil <me@davidzadrazil.cz>
2020-04-27 12:21:49 +0200

    Deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t davidzadrazil/multi-client:latest -t davidzadrazil/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t davidzadrazil/multi-server:latest -t davidzadrazil/multi-server:$SHA -f ./client/Dockerfile ./server
-docker build -t davidzadrazil/multi-worker:latest -t davidzadrazil/multi-worker:$SHA -f -f ./client/Dockerfile ./worker
+docker build -t davidzadrazil/multi-server:latest -t davidzadrazil/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t davidzadrazil/multi-worker:latest -t davidzadrazil/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push davidzadrazil/multi-client:latest
 docker push davidzadrazil/multi-server:latest
@@ -12,5 +12,5 @@ docker push davidzadrazil/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=davidzadrazil/multi-server:$SHA
-kubectl set image deployments/client-deployment server=davidzadrazil/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=davidzadrazil/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=davidzadrazil/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=davidzadrazil/multi-worker:$SHA
